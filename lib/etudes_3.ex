253c0869c6904d2c6b913f6c114f9548b3396004
Sebastian Gebski <sebastian@gebski.net>
2015-11-08 23:05:46 +0100

    Calling private + splitting tuple

--- lib/etudes_3.ex
+++ lib/etudes_3.ex
@@ -3,28 +3,30 @@ defmodule Etudes3 do
     Etudes from chapter #3
   """
 
-  @doc """
-    Area of rectangle
-  """
   @spec area(atom(), number(), number()) :: number()
-  def area(:rectangle, x, y) when x > 0 and y > 0 do
+  defp area(:rectangle, x, y) when x > 0 and y > 0 do
     x * y
   end
 
-  @doc """
-    Area of triangle
-  """
   @spec area(atom(), number(), number()) :: number()
-  def area(:triangle, a, h) when a > 0 and h > 0 do
+  defp area(:triangle, a, h) when a > 0 and h > 0 do
     (a * h) / 2.0
   end
 
-  @doc """
-    Area of ellipse
-  """
   @spec area(atom(), number(), number()) :: number()
-  def area(:ellipse, a, b) when a > 0 and b > 0 do
+  defp area(:ellipse, a, b) when a > 0 and b > 0 do
     :math.pi * a * b
+  end
+
+  defp area(_, _, _) do
+    0
+  end
+
+  @doc """
+    Area of whatever
+  """
+  def area({a,b,c}) do
+    area(a,b,c)
   end
 
 end
--- test/etudes_3_test.exs
+++ test/etudes_3_test.exs
@@ -3,39 +3,53 @@ defmodule Etudes3Test do
   doctest Etudes3
 
   test "area of rectangle 4 x 5" do
-    assert Etudes3.area(:rectangle,4,5) == 20
+    assert Etudes3.area({:rectangle,4,5}) == 20
   end
 
   test "rectangle dimensions should be positive #1" do
-    assert_raise FunctionClauseError, fn -> Etudes3.area(:rectangle,-1,5) end
+    # assert_raise FunctionClauseError, fn -> Etudes3.area(:rectangle,-1,5) end
+    assert Etudes3.area({:rectangle,-1,5})
   end
 
   test "rectangle dimensions should be positive #2" do
-    assert_raise FunctionClauseError, fn -> Etudes3.area(:rectangle,4,-1) end
+    # assert_raise FunctionClauseError, fn -> Etudes3.area(:rectangle,4,-1) end
+    assert Etudes3.area({:rectangle,4,-1}) == 0
   end
 
   test "area of triangle 4 x 5" do
-    assert Etudes3.area(:triangle,4,5) == 10
+    assert Etudes3.area({:triangle,4,5}) == 10
   end
 
   test "triangle dimensions should be positive #1" do
-    assert_raise FunctionClauseError, fn -> Etudes3.area(:triangle,-1,5) end
+    # assert_raise FunctionClauseError, fn -> Etudes3.area(:triangle,-1,5) end
+    assert Etudes3.area({:triangle,-1,5})
   end
 
   test "triangle dimensions should be positive #2" do
-    assert_raise FunctionClauseError, fn -> Etudes3.area(:triangle,4,-1) end
+    # assert_raise FunctionClauseError, fn -> Etudes3.area(:triangle,4,-1) end
+    assert Etudes3.area({:triangle,4,-1}) == 0
   end
 
   test "area of ellipse 4 x 5" do
-    assert Etudes3.area(:ellipse,4,5) == :math.pi * 4 * 5
+    assert Etudes3.area({:ellipse,4,5}) == :math.pi * 4 * 5
   end
 
   test "ellipse dimensions should be positive #1" do
-    assert_raise FunctionClauseError, fn -> Etudes3.area(:ellipse,-1,5) end
+    # assert_raise FunctionClauseError, fn -> Etudes3.area(:ellipse,-1,5) end
+    assert Etudes3.area({:ellipse,-1,5}) == 0
   end
 
   test "ellipse dimensions should be positive #2" do
-    assert_raise FunctionClauseError, fn -> Etudes3.area(:ellipse,4,-1) end
+    # assert_raise FunctionClauseError, fn -> Etudes3.area(:ellipse,4,-1) end
+    assert Etudes3.area({:ellipse,4,-1}) == 0
+  end
+
+  test "area of whatever 4 5" do
+    assert Etudes3.area({:whatever,4,5}) == 0
+  end
+
+  test "calling private is not cool" do
+    assert catch_error Etudes3.area(:triangle,4,5) == UndefinedFunctionError
   end
 
 end
