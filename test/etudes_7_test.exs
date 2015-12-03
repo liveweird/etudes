3da4d3f1b0658d6703fe8b87be0c77269db3c195
Sebastian Gebski <sebastian@gebski.net>
2015-12-03 17:32:46 +0100

    Another test - cities in country

--- test/etudes_7_test.exs
+++ test/etudes_7_test.exs
@@ -27,4 +27,22 @@ defmodule Etudes7Test do
     end) == true
   end
 
+  test "there are 3 cities in Germany: Hamburg, Frankfurt & Dresden" do
+    countries = Etudes7.make_geo_list("geography.csv")
+    names = [ "Hamburg", "Frankfurt", "Dresden" ]
+    germany = countries |> Enum.find(fn (country) -> country[:name] == "Germany" end)
+    cities = germany[:cities] |> Enum.map(fn (elem) -> elem.name end) |> Enum.uniq |> Enum.sort
+    zipped = Enum.zip(cities, names)
+    assert Enum.count(zipped) == 3
+    assert Enum.all?(zipped, fn (el) -> elem(el, 0) == elem(el, 1) end) == true
+  end
+
+  test "Granada's population is 234325"
+
+  test "Daegu's latitude is 35.87028"
+
+  test "Lima's longitude is -77.02824"
+
+  test "Valencia is not in Spain"
+
 end
