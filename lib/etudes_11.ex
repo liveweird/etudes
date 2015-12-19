ae7b4400c470e7c8e75984e45e346ab93606875e
Sebastian Gebski <sebastian@gebski.net>
2015-12-19 11:21:03 +0100

    Boilerplate for ETS module

---
+++ lib/etudes_11.ex
@@ -0,0 +1,9 @@
+defmodule Etudes11 do
+  @moduledoc """
+    Chapter #11 - ETS
+  """
+
+  require Logger
+  require IEx
+
+end
---
+++ test/etudes_11_test.exs
@@ -0,0 +1,8 @@
+defmodule Etudes11Test do
+  use ExUnit.Case
+  doctest Etudes11
+
+  require Logger
+  require IEx
+
+end
