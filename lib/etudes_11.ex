80056e0f26848df16e2db51ea7ec87edfad6aae9
Sebastian Gebski <sebastian@gebski.net>
2015-12-20 11:14:31 +0100

    Reading file & putting the content in ETS as a map of lists

--- lib/etudes_11.ex
+++ lib/etudes_11.ex
@@ -6,4 +6,31 @@ defmodule Etudes11 do
   require Logger
   require IEx
 
+  defmodule PhoneCall do
+    defstruct start_date: "1900-01-01", start_time: "00:00:00", end_date: "1900-01-01", end_time: "00:00:00"
+  end
+
+  @doc """
+    Read phone call database into malleable format
+  """
+  @spec setup(String.t()) :: atom()
+  def setup(file_name) do
+    :ets.new(:phone_call_registry, [:set, :protected, :named_table])
+    File.stream!(file_name, [:read, :utf8], :line) |>
+    Enum.each(fn (line) ->
+      atoms = line |> to_string |> String.split(",") |> Enum.map(&String.strip(&1))
+      [ number, start_date, start_time, end_date, end_time ] = atoms
+      temp_calls = :ets.lookup(:phone_call_registry, number)
+      :ets.insert(:phone_call_registry, temp_calls ++ [ %PhoneCall{ start_date: start_date, start_time: start_time, end_date: end_date, end_time: end_time } ])
+    end)
+    :phone_call_registry
+  end
+
+  @doc """
+    Get all numbers in the book
+  """
+  @spec get_numbers() :: list()
+  def get_numbers do
+  end
+
 end
--- test/etudes_11_test.exs
+++ test/etudes_11_test.exs
@@ -5,4 +5,14 @@ defmodule Etudes11Test do
   require Logger
   require IEx
 
+  test "Input file properly read" do
+    registry = Etudes11.setup("./test/call_data.csv")
+    numbers = Etudes11.get_numbers
+    assert Enum.count(numbers) == 7
+  end
+
+  test "All phone numbers present"
+
+  test "All phone calls for particular phone numbers present"
+
 end
