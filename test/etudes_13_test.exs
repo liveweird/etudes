6d1dd4fbe50faa4125fd2e1319c88d8d4ae4afe8
Sebastian Gebski <sebastian@gebski.net>
2016-01-11 13:18:17 +0100

    Added some tests

--- test/etudes_13_test.exs
+++ test/etudes_13_test.exs
@@ -7,6 +7,23 @@ defmodule Etudes13Test do
 
   import ExUnit.CaptureIO
 
-  test "Sample test"
+  test "Atomic weight of one atom" do
+    assert o == 15.999
+  end
+
+  test "Atomic weight of water" do
+    water = h * 2 + o
+    assert water == 18.015
+  end
+
+  test "Atomic weight of sulfuric acid" do
+    sulfuric_acid = h * 2 + s + o * 4
+    assert sulfuric_acid == 98.072
+  end
+
+  test "Atomic weight of salt" do
+    salt = na + cl
+    assert salt == 58.44
+  end
 
 end
